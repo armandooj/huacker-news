e58b67176ffcf2d511e531fd7a0bdcb3e238e99e
Armando Ochoa <armando.o.j@gmail.com>
2016-02-24 00:04:35 +0100

    Add a controller for comments

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ var app = angular.module('huackerNews', ['ui.router']);
 app.factory('posts', function() {
     var o = {
         posts: [
-            {title: 'German government to use Trojan spyware to monitor citizens', upvotes: 5},
-            {title: 'Million Dollar Curve', upvotes: 2},
-            {title: 'Bill and Melinda Gates 2016 Annual Letter', upvotes: 3},
-            {title: 'Why I don\'t want stuff', upvotes: 2},
-            {title: 'Show HN: Windows 98 in browser', upvotes: 4}
+            {
+                title: 'German government to use Trojan spyware to monitor citizens', 
+                upvotes: 3,
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Doe', body: 'Pff, horrible post.', upvotes: 1}
+                ]
+            },
+            {
+                title: 'Million Dollar Curve',
+                upvotes: 5,
+                comments: [
+                    {author: 'Mark', body: 'Thank you', upvotes: 2},
+                    {author: 'Bob', body: 'This is a comment.', upvotes: 0}
+                ]
+            }
         ]
     };
     return o;
@@ -20,6 +31,7 @@ app.controller('MainCtrl', [
 function($scope, posts) {
     $scope.test = 'Hello World';
     $scope.posts = posts.posts;
+    
     $scope.addPost = function() {
         if (!$scope.title || $scope.title === '') {
             return; 
@@ -27,13 +39,37 @@ function($scope, posts) {
         $scope.posts.push({
             title: $scope.title,
             link: $scope.link,
-            upvotes: 0
+            upvotes: 0,
+            comments: [
+                {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                {author: 'Doe', body: 'Pff, horrible post.', upvotes: 0}
+            ]
         });
         $scope.title = '';
         $scope.link = '';
     };
     $scope.incrementUpvotes = function(post) {
         post.upvotes += 1;
+    }  
+}]);
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {  
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function() {
+        if ($scope.body === '') {
+            return;
+        }
+        $scope.post.comments.push({
+            author: 'user',
+            body: $scope.body,
+            upvotes: 0
+        });
+        $scope.body = '';
     }
 }]);
 
@@ -41,10 +77,16 @@ app.config([
     '$stateProvider',
     '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('home', {
+        $stateProvider
+        .state('home', {
             url: '/home',
             templateUrl: '/index.html',
             controller: 'MainCtrl'
+        })
+        .state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl'
         });
 
         $urlRouterProvider.otherwise('home');
