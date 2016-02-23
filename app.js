12042c7f049feadf0928a8227bddd496a71ae20a
Armando Ochoa <armando.o.j@gmail.com>
2016-02-23 20:34:34 +0100

    Move the posts to a service

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 var app = angular.module('huackerNews', []);
 
+// Declare a posts service
+app.factory('posts', function() {
+    var o = {
+        posts: [
+            {title: 'German government to use Trojan spyware to monitor citizens', upvotes: 5},
+            {title: 'Million Dollar Curve', upvotes: 2},
+            {title: 'Bill and Melinda Gates 2016 Annual Letter', upvotes: 3},
+            {title: 'Why I don\'t want stuff', upvotes: 2},
+            {title: 'Show HN: Windows 98 in browser', upvotes: 4}
+        ]
+    };
+    return o;
+});
+
 app.controller('MainCtrl', [
 '$scope',
-function($scope) {
+'posts', // Injecting the service
+function($scope, posts) {
     $scope.test = 'Hello World';
-    $scope.posts = [
-        {title: 'German government to use Trojan spyware to monitor citizens', upvotes: 5},
-        {title: 'Million Dollar Curve', upvotes: 2},
-        {title: 'Bill and Melinda Gates 2016 Annual Letter', upvotes: 3},
-        {title: 'Why I don\'t want stuff', upvotes: 2},
-        {title: 'Show HN: Windows 98 in browser', upvotes: 4}
-    ];
+    $scope.posts = posts.posts;
     $scope.addPost = function() {
         if (!$scope.title || $scope.title === '') {
             return; 
