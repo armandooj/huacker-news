a8a66a9db9b0b00acb768a1ead66244cb592cca6
Armando Ochoa <armando.o.j@gmail.com>
2016-02-27 21:36:29 +0100

    Basic backend for comments and posts

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
+
+require('./models/Posts')
+require('./models/Comments')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -55,6 +59,9 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+// open a connection with the mongo server
+mongoose.connect('mongodb://localhost/news');
 
 
 module.exports = app;
